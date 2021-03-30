148a34dc5d8e9b6d62b7b63ab82597e8ba5c6a04
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-03-30 20:19:21 +0100

    Clase masa hecha y funcionando

---
+++ src/ejercicio-2/masa.ts
@@ -0,0 +1,32 @@
+import {isConvertible} from './isConvertible';
+
+export type massUnits = "gramos" | "libras";
+
+export class Masa implements isConvertible<massUnits> {
+  /**
+   * Constructor de la clase
+   */
+  constructor(public amount: number, public unitTo: massUnits) {
+  }
+  /**
+   * Funcion para la conversion de unidades de masa
+   * @param amount Valor de la unidad
+   * @param unitTo Unidad a la que deseo convertir
+   * @returns Un string con el resultado de la conversion
+   */
+
+  conversor(): string {
+    let output: string = "";
+
+    if (this.unitTo === "libras") {
+      output = (`[${this.amount.toString()}] gramos es igual a [${this.amount * 0.00220462}] libras`);
+      return output;
+    } else {
+      output = (`${this.amount.toString()} libras es igual a ${this.amount * 1.60934} gramos`);
+      return output;
+    }
+  }
+}
+
+const masa: Masa = new Masa(100, "libras");
+console.log(masa.conversor());

50cd88c57316fe05c89206cb2b8732cafdcd4aaa
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-03-30 18:23:27 +0100

    Clase velocidad hecha y funcionando

--- src/ejercicio-2/isConvertible.ts
+++ src/ejercicio-2/isConvertible.ts
@@ -1,13 +1,10 @@
 /**
  * Interfaz generica para la conversión
  */
+
 export interface isConvertible<T> {
-  unit: number;
+  amount: number;
   unitTo: T;
-  /**
-   * Conversión de las unidades
-   * @param unit Unidad a convertir
-   * @param unitTo Parametro para ver a que unidad se convierte
-   */
-  conversor(unit: number, unitTo: T): string;
+
+  conversor(): string;
 }
---
+++ src/ejercicio-2/velocidad.ts
@@ -0,0 +1,32 @@
+import {isConvertible} from './isConvertible';
+
+export type speedUnits = "millas" | "kilometros";
+
+export class Velocidad implements isConvertible<speedUnits> {
+  /**
+   * Constructor de la clase
+   */
+  constructor(public amount: number, public unitTo: speedUnits) {
+  }
+  /**
+   * Funcion para la conversion de unidades de velocidad
+   * @param amount Valor de la unidad
+   * @param unitTo Unidad a la que deseo convertir
+   * @returns Un string con el resultado de la conversion
+   */
+
+  conversor(): string {
+    let output: string = "";
+
+    if (this.unitTo === "millas") {
+      output = (`[${this.amount.toString()}] kilometros/hora es igual a [${this.amount * 0.621371}] millas/hora`);
+      return output;
+    } else {
+      output = (`${this.amount.toString()} millas/hora es igual a ${this.amount * 1.60934} kilometros/hora`);
+      return output;
+    }
+  }
+}
+
+const velocidad: Velocidad = new Velocidad(100, "millas");
+console.log(velocidad.conversor());
